3261f10b8c0e45ca7f26c11b8f5ae4a82253c3bc
Polly Michel <pmichel928@gmail.com>
2019-11-13 18:38:58 -0500

    Started working on React markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
+
+const origText = '# Markdown Previewer\nThis is a Markdown previewer built with React and the marked package' +
+  '(https://cdnjs.com/libraries/marked). You can ' +
+  'write in **bold**, _italic_, or _**both**_! \nIn addition to the header above, you can write\n## Sub-Headers\n' +
+  '### and Sub-Sub-Headers'
+
+class TextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: origText
+    };
+    this.updateText = this.updateText.bind(this);
+    this.markdownText = this.markdownText.bind(this);
+  }
+  updateText(event) {
+    this.setState({
+      input: event.target.value
+    })
+  }
+  markdownText() {
+    const markedText = marked(this.state.input, { sanitize: true });
+    return { __html: markedText };
+  }
+  render(){
+    return (
+      <div>
+        <textarea id="markdown-text" value={this.state.input} onChange={this.updateText}></textarea>
+        <p>Input text:</p>
+        <p dangerouslySetInnerHTML={this.markdownText()}></p>
+        <img src={logo} alt="Logo" height="100px" width="100px" />
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextInput />
     </div>
   );
 }
