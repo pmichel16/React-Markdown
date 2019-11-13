955ac70bf3c300c880d7fe37aa51025da84df905
Polly Michel <pmichel928@gmail.com>
2019-11-13 14:44:27 -0500

    Updates

---
+++ precache-manifest.b48f84a84da5d121ad1cc792dfa2e67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c11783789d61c217975d9bf8786840f",
+    "url": "/React-Markdown/index.html"
+  },
+  {
+    "revision": "e6395f1d7d6f542f292e",
+    "url": "/React-Markdown/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5345793fc15f15418d47",
+    "url": "/React-Markdown/static/js/2.e45c036f.chunk.js"
+  },
+  {
+    "revision": "e6395f1d7d6f542f292e",
+    "url": "/React-Markdown/static/js/main.e7746586.chunk.js"
+  },
+  {
+    "revision": "f24224b40bc9f3ee9461",
+    "url": "/React-Markdown/static/js/runtime-main.6e0062ca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/React-Markdown/static/media/logo.25bf045c.svg"
+  }
+]);
